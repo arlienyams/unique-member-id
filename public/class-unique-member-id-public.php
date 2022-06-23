ef74c47fdd48a6773ce48702dd0a8405cd57a4a0
Arlington <arlienyams.an@gmail.com>
2022-06-23 11:57:34 +0200

    edit Readme

--- public/class-unique-member-id-public.php
+++ public/class-unique-member-id-public.php
@@ -102,8 +102,7 @@ class Unique_Member_Id_Public
 	}
 }
 
-// show user id in profile and in shortcode
-// Add Shortcode
+// Show user id in profile and in shortcode 
 function uniqueID_shortcode()
 {
 
@@ -122,7 +121,7 @@ add_shortcode('nyams_uniqueID', 'uniqueID_shortcode');
 
 
 /*
- * Adding the column
+ * Add column
  */
 function nyams_user_id_column($columns)
 {
@@ -137,7 +136,10 @@ add_filter('manage_users_columns', 'nyams_user_id_column');
 function nyams_user_id_column_content($value, $column_name, $user_id)
 {
 	if ('user_id' == $column_name)
-		return $user_id;
+
+		$user_id =  str_pad($user_id, 4, "0", STR_PAD_LEFT);
+
+	return $user_id;
 	return $value;
 }
 add_action('manage_users_custom_column',  'nyams_user_id_column_content', 10, 3);
@@ -145,8 +147,8 @@ add_action('manage_users_custom_column',  'nyams_user_id_column_content', 10, 3)
 /*
  * Column style (you can skip this if you want)
  */
-function nyams_user_id_column_style()
-{
-	echo '<style>.column-user_id{width: 10%}</style>';
-}
-add_action('admin_head-users.php',  'nyams_user_id_column_style');
+// function nyams_user_id_column_style()
+// {
+// 	echo '<style>.column-user_id{width: 10%}</style>';
+// }
+// add_action('admin_head-users.php',  'nyams_user_id_column_style');
